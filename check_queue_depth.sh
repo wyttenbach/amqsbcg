a5e9b1d33029e448c8fccab3551f85428a2900c4
Websphere Service Account <mqm@vaausvrsappdev106.aac.va.gov>
2024-06-06 09:56:51 -0500

    init

---
+++ check_queue_depth.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+QUEUE_NAME=SYSTEM.MANAGED.DURABLE.66479F362094C401
+QUEUE_MANAGER=vrm.queue.manager
+TEMP_FILE="mq_messages.tmp"
+
+ /opt/mqm/samp/bin/amqsbcg $QUEUE_NAME $QUEUE_MANAGER > $TEMP_FILE
+
+OLDEST_DATE=$(grep -m 1 "PutDate" $TEMP_FILE | awk '{print $3}')
+OLDEST_TIME=$(grep -m 1 "PutTime" $TEMP_FILE | awk '{print $3}')
+
+
+
+echo "Oldest message put date: $OLDEST_DATE"
+echo "Oldest message put time: $OLDEST_TIME"
