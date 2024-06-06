241da8fac859e28010b32991eddcfceb38881e11
Websphere Service Account <mqm@vaausvrsappdev106.aac.va.gov>
2024-06-06 15:08:45 -0500

    oops

--- count.sh
+++ count.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . common.sh
 
-/opt/mqm/samp/bin/amqsbcg $QUEUE_NAME $QUEUE_MANAGER | awk '/PutDate/ { print $3 }' | sort | uniq wc -l
+/opt/mqm/samp/bin/amqsbcg $QUEUE_NAME $QUEUE_MANAGER | awk '/PutDate/ { print $3 }' | sort | uniq | wc -l
