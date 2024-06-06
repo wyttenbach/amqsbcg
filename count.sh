3fbce1df5cbbcfdcb23dc954b5edf57175a0398f
Websphere Service Account <mqm@vaausvrsapp50.aac.va.gov>
2024-06-06 18:27:41 -0500

    line count

--- count.sh
+++ count.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 . common.sh
 
-/opt/mqm/samp/bin/amqsbcg $QUEUE_NAME $QUEUE_MANAGER | awk '/PutDate/ { print $3 }' | sort | uniq | wc -l
+/opt/mqm/samp/bin/amqsbcg $QUEUE_NAME $QUEUE_MANAGER > $TEMP_FILE
+wc -l $TEMP_FILE
+awk '/PutDate/ { print $3 }' $TEMP_FILE | sort | uniq | wc -l
