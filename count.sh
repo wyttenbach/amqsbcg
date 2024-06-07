33ad234383e54c4349d940609280ca5ce30ca505
Websphere Service Account <mqm@vaausvrsapp50.aac.va.gov>
2024-06-06 19:22:22 -0500

    cleanup

--- count.sh
+++ count.sh
@@ -3,7 +3,7 @@
 
 /opt/mqm/samp/bin/amqsbcg $QUEUE_NAME $QUEUE_MANAGER > $TEMP_FILE
 
-exe() { echo "\$ ${@/eval/}" ; "$@" ; }
+exe() { echo "\$${@/eval/}" ; "$@" ; }
 
 exe wc -l $TEMP_FILE
 exe eval "awk '/PutDate/ { print \$3 }' $TEMP_FILE | wc -l"
