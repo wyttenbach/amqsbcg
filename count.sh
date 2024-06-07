099281f72b42d815589ffabb839f77e5d7e067d9
Websphere Service Account <mqm@vaausvrsapp50.aac.va.gov>
2024-06-06 19:19:15 -0500

    exe eval

--- count.sh
+++ count.sh
@@ -2,5 +2,9 @@
 . common.sh
 
 /opt/mqm/samp/bin/amqsbcg $QUEUE_NAME $QUEUE_MANAGER > $TEMP_FILE
-wc -l $TEMP_FILE
-awk '/PutDate/ { print $3 }' $TEMP_FILE | sort | uniq | wc -l
+
+exe() { echo "\$ ${@/eval/}" ; "$@" ; }
+
+exe wc -l $TEMP_FILE
+exe eval "awk '/PutDate/ { print \$3 }' $TEMP_FILE | wc -l"
+exe eval "awk '/PutDate/ { print \$3 }' $TEMP_FILE | sort | uniq | wc -l"
